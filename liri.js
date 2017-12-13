c111dc7b70c096c483916c5685f28fcf16e14802
Peter Le <Peter Le@LC-PETERLAPTOP.lec.corp.com>
2017-12-13 03:22:12 -0600

    completed homework

--- liri.js
+++ liri.js
@@ -11,7 +11,6 @@ userCommand = process.argv[2];
 userInput = process.argv[3];
 
 function twitter(){
-	console.log("Twitter Works!");
 	twitterFeed.get("statuses/user_timeline", function (error, tweets, response) {
       tweets.forEach(function(tweets){
          console.log("--------" + tweets.user.screen_name + "--------");
@@ -23,84 +22,105 @@ function twitter(){
 };
 
 function searchSpotify(){
-	console.log("Spotify Works!")
+	inputArray = process.argv;
+	songName = "";
 
-	spotify.search({ type: 'track', query: 'All the Small Things'}, function(err, data) {
+	for (var i = 3; i < inputArray.length; i++) {
+		if (i > 3 && i < inputArray.length) {
+			songName = songName + "+" + inputArray[i];
+		}
+		else {
+			songName += inputArray[i];
+		}
+	}
+
+	spotify.search({ type: 'track', query: songName, limit: 5}, function(err, data) {
 	  	if (err) {
 	    	return console.log('Error occurred: ' + err);
 	  	}
-	console.log(data.tracks.items[0].artists); 
+			data.tracks.items.forEach(function(items){
+				console.log("--------------------------")
+				console.log("Song name: " + items.name);
+				console.log("Artist(s): " + items.artists[0].name);
+				console.log("Song Album: " + items.album.name);
+				console.log("Preview Link: " + items.preview_url);
+				console.log("--------------------------")
+			});
 	});
-
-
-
-// Artist(s) ----- artists	
-
-// The song's name ----- userInput
-
-// A preview link of the song from Spotify ----- preview_url
-
-// The album that song is from ----- album_type
-
-// The album that the song is from ----- name
-
 };
 
 function movie(){
-	console.log("Movie Works!")
-	var queryURL = "http://www.omdbapi.com/?apikey=40e9cece&" + userInput;
-    // $.ajax({
-    //   url: queryURL,
-    //   method: "GET"
-    // }).done(function(response) {
-    // 	console.log("Movie Title: " + response.Title);
-    // 	console.log("Year Released: " + response.Year);
-    // 	console.log("IMDB Rating: " + response.imdbRating);
-    // 	console.log("Rotten Tomatoes Rating: " + response.Ratings[1].Value);
-    // 	console.log("Country Produced: " + response.Country);
-    // 	console.log("Movie Language: " + response.Language);
-    // 	console.log("Movie Plot: " + response.Plot);
-    // 	console.log("Main Actors: " + response.Actors);
-    // });       
+	inputArray = process.argv;
+	movieName = "";
+
+	for (var i = 3; i < inputArray.length; i++) {
+	  if (i > 3 && i < inputArray.length) {
+	    movieName = movieName + "+" + inputArray[i];
+	  }
+	  else {
+	    movieName += inputArray[i];
+	  }
+	}
+
+	var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+	request(queryURL, function(error, response, body) {
+
+	  if (!error && response.statusCode === 200) {
+			console.log("--------------------------")
+	    console.log("Movie Titie: " + JSON.parse(body).Title);
+			console.log("Year Released: " + JSON.parse(body).Year);
+			console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+			console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+			console.log("Country Produced: " + JSON.parse(body).Country);
+			console.log("Movie Language: " + JSON.parse(body).Language);
+			console.log("Movie Plot: " + JSON.parse(body).Plot);
+			console.log("Main Actors: " + JSON.parse(body).Actors);
+			console.log("--------------------------")
+	  }
+	});
+
 }
 
 function textFile(){
 	console.log("File Works!")
-	// var fs = require("fs");
-	// fs.readFile("random.txt", "utf8", function(err, data) {
-	//   	if (err) {
-	//    	return console.log(err);
-	//   	}
+	var fs = require("fs");
+	fs.readFile("random.txt", "utf8", function(err, data) {
+	  	if (err) {
+	   	return console.log(err);
+	  	}
 
-	//   	var output = data.split(",");
+	  	var textFileArray = data.split(",");
 
-	//   	for (var i = 0; i < output.length; i++) {
-	//    	console.log(output[i]);
-	//   	}
-	// });
+	  	userCommand = textFileArray[0];
+			userInput = textFileArray[1];
+
+			runProgram();
+	});
 }
 
 function error(){
 	console.log("Invalid Input!")
 };
 
+function runProgram(){
+	switch (userCommand) {
 
+		case "my-tweets":
+			twitter();
+			break;
+		case "spotify-this-song":
+			searchSpotify();
+			break;
+		case "movie-this":
+			movie();
+			break;
+		case "do-what-it-says":
+			textFile();
+			break;
+		default:
+			error();
+			break;
+	}
+};
 
-switch (userCommand) {
-
-	case "my-tweets":
-		twitter();
-		break;
-	case "spotify-this-song":
-		searchSpotify();
-		break;
-	case "movie-this":
-		movie();
-		break;
-	case "do-what-it-says":
-		textFile();
-		break;
-	default:
-		// error();
-		break;
-}
+runProgram();
